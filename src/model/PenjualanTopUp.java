ba873f2f75b7fb05d6e6807005531ce40afe0451
ASUS <ASUS@LAPTOP-M3KPU28B>
2021-10-28 20:53:41 +0700

    add Enum TypeTopUp,enum JenisPemabayaran, topUp , Penjualan TopUp

---
+++ src/model/PenjualanTopUp.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author ASUS
+ */
+public class PenjualanTopUp {
+    private String Nomor_FakturTopUp;
+    private double Total_PenjualanTopUp;
+    private Enum Jenis_Pembayaran;
+    
+}
