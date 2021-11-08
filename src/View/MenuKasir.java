bbd3984ff90271d14ed8aceafecb6512abb1f6ed
HP <HP@DESKTOP-LTBNAVE>
2021-11-08 21:28:31 +0700

    menambahMenuKasirDanFormulirPenjualanBarang

---
+++ src/View/MenuKasir.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author HP
+ */
+public class MenuKasir {
+    private int menu;
+    
+    public MenuKasir(){
+        
+    }
+    
+    private void TampilanMenuKasir(){
+        do {
+            menu = Integer.parseInt(JOptionPane.showInputDialog(null,"Selamat datang kasir" + "Nama kasir"
+                    + "\n1. "));
+        } while (true);
+        
+    }
+}
